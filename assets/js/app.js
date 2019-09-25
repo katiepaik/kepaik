41721b312b7f19696d8f504c55bfc6ae3b9758e4
Katie <paikk662@newschool.edu>
2019-09-25 14:06:16 -0400

    updates

---
+++ assets/js/app.js
@@ -0,0 +1,87 @@
+$(".typoclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".commuteclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".apiclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".ckaclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".plantclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".rockclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".chalazaclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".postclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".alphaclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".ironclick").click(function(){
+    $(".more").css("display", "block");
+    $(".name").css("display", "none");
+    $(".about").css("display", "none");
+    $(".work").css("display", "none");
+    $(".conwrapper").css("display", "none");
+});
+
+$(".xout").click(function(){
+    $(".more").css("display", "none");
+    $(".name").css("display", "inline-block");
+    $(".about").css("display", "inline-block");
+    $(".work").css("display", "inline-block");
+    $(".conwrapper").css("display", "flex");
+});
